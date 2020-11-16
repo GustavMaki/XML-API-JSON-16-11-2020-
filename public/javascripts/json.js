3f5685ffeb0729d3a2012ed2f5894df88dce7c88
GustavMaki <gustav.siljamaki@gmail.com>
2020-11-16 16:02:17 +0100

    Added button that prints out notes (content from JSON API), small amout of styling (done in the html tags), changes Express to Notes in titles.

--- public/javascripts/json.js
+++ public/javascripts/json.js
@@ -1,3 +1,6 @@
+document.querySelector("button").addEventListener("click", e => {
+    e.preventDefault();
+
 var myXHR = new XMLHttpRequest();
 
 var MyNotes ={
@@ -8,9 +11,22 @@ var MyNotes ={
 myXHR.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200) {
         MyNotes = JSON.parse(this.responseText);
+        Printdata();
     }
 };
 
 myXHR.open("GET", "/users", true);
 
 myXHR.send();
+
+function Printdata(){
+    var pagedata ="";
+    var i;
+    for (i=0; i<MyNotes.length; i++){
+        pagedata+= "<li>" + MyNotes[i].title + "  |  " + MyNotes[i].content + "</li>"
+    }
+
+    document.getElementById("result").innerHTML = pagedata;
+}
+
+});
